1cdeee2be245b2af702e23366642d3a0276c4ce5
James Bradbury <jamesbradbury93@gmail.com>
2018-10-20 00:12:52 +0100

    CreatDir and GlobalVariables class created instead of global variables

--- cc_util.py
+++ cc_util.py
@@ -1,14 +1,16 @@
 import os
 import re
+import pandas as pd
 
-class c_dir:
-    def __init__(self, output):
+class CreateDir:
+    def __init__(self, obj):
         self.new_dir = ''
         self.phrase_num = 1
         self.max = 0
-        self.phrase_dir = os.listdir(output)
+        self.phrase_dir = os.listdir(obj.output)
 
-    def make(self, output):
+        ### The rest makes a new directory ###
+        ### No point having it in a separate function because if you're making an instance of CreateDir you are going to want a new directory...###
         self.max = 0
         for item in self.phrase_dir:
             if not item.startswith('.'):
@@ -18,13 +20,27 @@ class c_dir:
         self.max += 1
         self.max = str(self.max)
         self.phrase_num = f'phrases_{self.max}'
-        self.new_dir = f'{output}{self.phrase_num}/'
+        self.new_dir = f'{obj.output}{self.phrase_num}/'
         if not os.path.exists(self.new_dir):
             os.makedirs(self.new_dir)
-        print(self.new_dir)
+        print(f'New directory:\n{self.new_dir}')
+
+class GlobalVariables:
+    def __init__(self):
+        self.this_path = get_path()
+        self.source = f'{self.this_path}/voice_sounds/' # Sample Databases
+        self.output = f'{self.this_path}/phrases/' # Output Folder
+        self.meta_path = f'{self.this_path}/samples/' # Simplify path finding for Meta Behaviour Class
+        self.affix = '.wav' # Extension
+        self.db_path = f'{self.this_path}/db.csv'
+        self.df = pd.read_csv(self.db_path)
+        self.df_len = self.df.shape[0]
+        self.amp = self.df.AMP
+        self.centroid = self.df.CENTROID
+        self.duration = self.df.DURATION
 
 def input_helper():
-    user_input = input('Please enter a descriptor \n')
+    user_input = input('Please enter a descriptor\n Examples\n amp > -27 centroid > 0 duration < 500\n amp > -27 centroid > 0 duration < 500\n')
     return user_input
 
 def translate(value, in_lo, in_hi, out_lo, out_hi):

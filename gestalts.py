4c27c617e864b021dbdc75c15246be5cc24264d0
James Bradbury <jamesbradbury93@gmail.com>
2018-11-01 17:45:35 +0000

    refactoring of gestalts

--- gestalts.py
+++ gestalts.py
@@ -3,51 +3,42 @@ from cc_data import EntryMatcher
 import cc_util as util
 from pydub import AudioSegment
 
-def law_of_proximity(iterations, divisions):
+def law_of_proximity(iterations, divisions, obj, drs):
 
     # start with an initial unit which is radically established as a whole. Sub units are developed from this by increasing the proximity between sub-sets. Proximity is relative, so a group can be distinguished by its proximity to another group. Or the same group of samples can form two different gestalt units by having different internal proximities.
 
     sequence = []
-    # dr = c_dir()
-    # dr.make()
 
-    gos = EntryMatcher()
-    gos.input_vars('amp <-> 6 -54 centroid <-> 90 4968 duration < 500')
-    gos.match()
+    sample_set = EntryMatcher()
+    sample_set.input_vars('amp <-> 6 -54 centroid <-> 90 4968 duration < 500')
+    sample_set.match(obj)
 
     
-    for x in range(iterations):
-        concat = AudioSegment.empty()
-        rand_div      = [0] * divisions
-        rand_div_norm = [0] * divisions
-        rand_div_sum = 0
 
-        for r in range(divisions):
-            rand_div[r] = rn.randint(0, 100)
 
-        rand_div_sum = sum(rand_div)
+def law_of_continuity(iterations, joins, obj, drs):
+    """Plays back all samples in index order
+    
+    Keyword arguments:
 
-        for a in range(divisions):
-            rand_div_norm[a] = round( ((rand_div[a] / rand_div_sum) * gos.match_len), 0)
-        
-        print (sum(rand_div_norm))
-        print (gos.match_len)
-        print (rand_div_norm)
-
-def law_of_continuity(iterations, joins):
-
-    dr = util.CreateDir(output)
-    dr.make(output)
-
-    gos = EntryMatcher()
-    gos.input_vars('amp > -27 centroid > 0 duration < 500')
-    gos.match(df_len)
+    iterations -- how many sets of concatenations
+    joins -- how many concatenations per iteration
+    obj -- pass the global variable class to reference directory and affix information
+    drs -- pass the name of the class that creates your new directory
+    
+    """
+    sample_set = EntryMatcher()
+    sample_set.input_vars(util.input_helper())
+    sample_set.match(obj)
 
     for x in range(iterations):
-        sample_1 = AudioSegment.from_file(source + str(rn.choice(gos.matcher_result)) + affix)
-        sample_2 = AudioSegment.from_file(source + str(rn.choice(gos.matcher_result)) + affix)
+        choice_1 = str(rn.choice(sample_set.match_result))
+        choice_2 = str(rn.choice(sample_set.match_result))
+        sample_1 = AudioSegment.from_file(f'{obj.source}{choice_1}{obj.affix}')
+        sample_2 = AudioSegment.from_file(f'{obj.source}{choice_2}{obj.affix}')
         concat = AudioSegment.empty()
         unit   = AudioSegment.empty()
+
         #find the biggest container
         if len(sample_1) > len(sample_2):
             unit += sample_1
@@ -60,5 +51,6 @@ def law_of_continuity(iterations, joins):
         for i in range(joins):
             concat += unit
             concat += AudioSegment.silent(duration = 1000)
-
-        concat.export(f'{dr.new_dir}{str(x)}{affix}', format="wav")
+        
+        num_iter = str(x)
+        concat.export(f'{drs.new_dir}{num_iter}{obj.affix}', format="wav")

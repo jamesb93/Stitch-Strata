d58e0d51c076e9cbecae561fa36052cdb67ab444
James Bradbury <jamesbradbury93@gmail.com>
2018-11-02 16:29:35 +0000

    added docstrings and a DataContainer() class

--- cc_data.py
+++ cc_data.py
@@ -1,3 +1,10 @@
+from pydub import AudioSegment
+import itertools
+import os
+from cc_util import norm_list
+import numpy as np
+import random as rn
+
 class EntryMatcher:
     def __init__(self):
         #amplitude variables
@@ -17,9 +24,23 @@ class EntryMatcher:
         
         #other variables for init
         self.results    = []
+        self.results_2  = []
+        self.results_3  = []
         self.input_list = []
         self.idx = 0
         self.match_len = 0
+
+        #normalisation and minmax values#
+        self.distance_array = []
+        self.match_amp_min = float("-inf")
+        self.match_amp_max = float("inf")
+        self.match_centroid_min = float("-inf")
+        self.match_centroid_max = float("inf")
+        self.match_duration_min = float("-inf")
+        self.match_duration_max = float("inf")
+        
+        #cull#
+        self.cull_limit = 0
 
         #meta data variables
         self.metadata = None
@@ -27,18 +48,25 @@ class EntryMatcher:
         #other
         self.descriptor_names = ['amp', 'centroid', 'duration']
 
-    def store_metadata(self, drs): #Create a text file inside directory with metadata
-        """Writes some metadata to a file inside the relevant phrase directory
+    def store_metadata(self, drs):
+        """
+        Writes some metadata to a file inside the relevant phrase directory
     
         Keyword arguments:
 
         drs -- pass the name of the class that creates your new directory
-        
         """
         self.metadata = open(f'{drs.new_dir}{drs.phrase_num}.txt', "a")
         self.metadata.write(self.inputs)
 
-    def input_vars(self, inputs): # parameters passed to instance of match()
+    def input_vars(self, inputs):
+        """
+        Helps you to input vars into an instance of the EntryMatcher() class
+
+        Arguments:
+
+        inputs -- input a list in the format {amp, centroid, duration} {>, <, <->}
+        """
         self.inputs = inputs
         self.input_list = self.inputs.split()
         try:
@@ -77,17 +105,13 @@ class EntryMatcher:
                     self.dur_oper = self.current_oper
 
     def match(self, obj): #Match Entries
-        """Performers matching
+        """
+        Match according to the criteria entered via .input_vars()
 
         Keyword arguments:
 
         obj -- pass the global variable class to reference the database
-        
         """
-
-        self.results = []
-        self.results_2 = []
-        self.results_3 = []
         if self.amp_oper == '>':
             for i in range (0, obj.df_len):
                 if (obj.amp[i] >= self.amp_test):
@@ -142,3 +166,104 @@ class EntryMatcher:
         self.match_result = list(self.results_3)
 
         self.match_len = len(self.match_result)
+
+    def cull(self, list_limit):
+        """
+        Shuffle a list, then slice it to make it smaller. Shuffling is so that you can get varied results
+
+        Arguments:
+
+        limit -- how small you want the list to be
+        """
+        self.cull_limit = int(list_limit)
+        self.match_result = rn.shuffle(self.match_result)
+        if len(self.match_result) < self.cull_limit:
+            print("You need to cull less than the length of the list")
+        else:
+            self.match_result = self.match_result[:self.cull_limit]
+
+
+class DataContainer:
+    """ 
+    A class that stores a big list of sequential samples. You can call the 'output' method on the instance to write all samples to an audio file.
+    """
+    def __init__(self):
+        self.big_list = [] # a big list to append to
+        self.concat = AudioSegment.empty() # a big container for audio files to be written to
+        self.sample = 0
+        self.prog = 0
+
+    def app(self, input_list):
+        """ 
+        Add a list to an instance of manager
+
+        Arguments:
+
+        input_list -- the list to add
+        """
+        for item in input_list:
+            if item is not None:
+                self.big_list.append(item)
+    
+    def output(self, obj, drs):
+        """ 
+        Write the internal list out to an audio file
+
+        Arguments:
+
+        obj - the global variables object (usually glovar)
+        drs - the directory object
+        """
+        for i in range(0, len(self.big_list)):
+            os.system("clear")
+            print((self.prog / len(self.big_list)) * 100.)
+            self.sample = self.big_list[i]
+            self.concat += AudioSegment.from_file(f'{obj.source}{self.sample}{obj.affix}')
+            self.prog += 1
+
+        self.concat.export(f'{drs.new_dir}iter_func{obj.affix}', format='wav') #export
+
+class MetaWrapper:
+    """
+    A class that calls functions according to a list you pass it
+    """
+    def __init__(self, entry_matcher_obj_1, entry_matcher_obj_2):
+        """
+        Init function
+
+        Arguments:
+
+        entry_matcher_obj_1 -- the first EntryMatcher() instance name you want to pass to this metawrapper
+        entry_matcher_obj_2 -- the second EntryMatcher() instance name you want to pass to this metawrapper
+        """
+        self.list_1 = entry_matcher_obj_1.match_result
+        self.list_2 = entry_matcher_obj_2.match_result
+    
+    def call_functions(self, function_list, obj):
+        """
+        A function to call other functions inside of a class
+
+        Arguments:
+
+        function_list -- a list of functions to call in sequential order
+        obj -- the name of the instance of the iterFunctions() class
+        """
+        for item in function_list:
+            if item == 0:
+                obj.zip_unzip(self.list_1, self.list_2) 
+                print("calling zip_unzip") 
+
+            elif item == 1:
+                obj.products(self.list_1, 2)
+                print("calling products")
+
+            elif item == 2:
+                obj.permute(self.list_1, 2)
+                print("calling permute")
+
+            elif item == 3:
+                obj.combinations(self.list_1, 2)
+                print("calling combinations")
+
+
+
